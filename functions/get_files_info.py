cdc70137934fff055f6964f70d1724a0bd513129
fortibit <86564794+fortibit@users.noreply.github.com>
2025-08-16 22:34:18 +0200

    Refactor within working dir check

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,11 +2,12 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
     full_path = os.path.join(working_directory, directory)
     abs_path = os.path.abspath(full_path)
     
     # Validate if the path is within working directory
-    if working_directory not in abs_path.split("/")[-2:]:
+    if not abs_path.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
     # Check if the directory argument is a directory
