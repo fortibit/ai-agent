1af49f1c9f545b54c220dd91bdbfff18438bac20
fortibit <86564794+fortibit@users.noreply.github.com>
2025-08-16 22:37:29 +0200

    Create absolute paths

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,10 @@
 import os
 
-
+#                    "calculator"     , "pkg/calculator.py"
+#                    "calculator"     , "main.py"
+#                    "calculator"     , "/bin/cat"               <== error str
+#                    "calculator"     , "pkg/does_not_exist.py"  <== error str
 def get_file_content(working_directory, file_path):
-    
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.join(abs_working_dir, file_path)
+    print(abs_file_path)
