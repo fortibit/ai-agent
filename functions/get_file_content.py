d479ca9d9962c2fdd8938e8c226937e667d1f4b2
fortibit <86564794+fortibit@users.noreply.github.com>
2025-08-16 22:47:19 +0200

    Add error handling

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,6 +5,16 @@ import os
 #                    "calculator"     , "/bin/cat"               <== error str
 #                    "calculator"     , "pkg/does_not_exist.py"  <== error str
 def get_file_content(working_directory, file_path):
+    # Create absolute paths
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.join(abs_working_dir, file_path)
-    print(abs_file_path)
+
+    return check_path(abs_working_dir, abs_file_path, file_path)
+
+
+def check_path(abs_working_dir_path, abs_file_path, file_path):
+    if not abs_file_path.startswith(abs_working_dir_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    pass
