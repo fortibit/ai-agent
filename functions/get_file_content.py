b67186f2345ee067c843d4a36e9d3c75ef07a4fd
fortibit <86564794+fortibit@users.noreply.github.com>
2025-08-17 08:41:30 +0200

    Add file reading to string

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,16 +1,20 @@
 import os
+from config import *
 
-#                    "calculator"     , "pkg/calculator.py"
-#                    "calculator"     , "main.py"
-#                    "calculator"     , "/bin/cat"               <== error str
-#                    "calculator"     , "pkg/does_not_exist.py"  <== error str
+
 def get_file_content(working_directory, file_path):
     # Create absolute paths
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.join(abs_working_dir, file_path)
 
-    return check_path(abs_working_dir, abs_file_path, file_path)
+    # Check for errors
+    error = check_path(abs_working_dir, abs_file_path, file_path)
 
+    try:
+        # Get file contents
+        return read_file(abs_file_path)
+    except Exception as e:
+        return f"Error: {e}"
 
 def check_path(abs_working_dir_path, abs_file_path, file_path):
     if not abs_file_path.startswith(abs_working_dir_path):
@@ -18,3 +22,11 @@ def check_path(abs_working_dir_path, abs_file_path, file_path):
     if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     pass
+
+
+def read_file(file_path):
+    with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+        if len(file_content_string) > MAX_CHARS:
+            file_content_string += '[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
